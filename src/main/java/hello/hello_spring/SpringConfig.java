807e5fef177b5e3b207145b7d9a9dd5c84c23208
SEEICANTSEE <leejieun.sw@gmail.com>
2025-03-04 20:36:47 +0900

    spring data jpa

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -11,31 +11,14 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource, EntityManager em){
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        // memberRepository는 인터페이스이고, MemoryMemberRepository가 구현체이므로
-        //return new MemoryMemberRepository();
-
-        //jdbc
-        //return new JdbcMemberRepository(dataSource);
-
-        //jdbcTemplate
-        //return new JdbcTemplateMemberRepository(dataSource);
-
-        //jpa
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
